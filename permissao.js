0931a5f08da5dd62c9af049f5ef0c225a21c6ae8
Shouganaii <frias.caio@yahoo.com.br>
2021-05-10 22:51:49 -0300

    refactor(permissao): troca de nomenclatura

--- permissao.js
+++ permissao.js
@@ -7,7 +7,7 @@ app.use((req, _res, next) => {
     next()
 })
 
-const proibe1 = (req, res, next) => {
+const proibeVisitante = (req, res, next) => {
     if (req.isAdmin == true) {
         next()
     } else {
@@ -19,7 +19,7 @@ app.get('/liberado', (req, res) => {
     res.send('Acesso permitido')
 })
 
-app.get('/proibido', proibe1, (req, res) => {
+app.get('/proibido', proibeVisitante, (req, res) => {
     res.send('Acesso permitido')
 })
 
